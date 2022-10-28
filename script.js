f6a07fb4c20294bfb22b31e1a96c5b632e7a07f7
Marie Lewis <mariea1022@gmail.com>
2022-10-28 16:50:07 -0700

    starting to add the rows and columns for the planner

---
+++ script.js
@@ -0,0 +1,15 @@
+// HOOKS to the DOM
+var currentDayEl = document.querySelector('#currentDay');
+
+// state variables
+
+// constant variables
+
+// handle displaying the current day
+// function currentDay() {
+//     var dayToday = moment().format('dddd, MMMM Do');
+//     console.log(dayToday)
+//     currentDayEl.text(dayToday)
+// }
+
+// setInterval(currentDay, 1000);
